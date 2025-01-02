f43c5d6b4ee62734d003525dddfc3df6db294eda
shihabshahrier <shahrier13@yahoo.com>
2025-01-02 18:51:22 +0600

    219. Contains Duplicate II

---
+++ 219. Contains Duplicate II.py
@@ -0,0 +1,10 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        seen = set()
+        for i, v in enumerate(nums):
+            if v in seen:
+                return True
+            seen.add(v)
+            if len(seen) > k:
+                seen.remove(nums[i-k])
+        return False
